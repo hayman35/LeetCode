7119fd33c7ca705f3b795d2d2013c0a11f957351
Hayman AbdelJabbar <hayman-35@hotmail.com>
2022-03-29 18:11:57 -0400

    adding ignore file

--- LeetCode.java
+++ LeetCode.java
@@ -1,14 +1,11 @@
 import java.util.Arrays;
 
 public class LeetCode {
-    public int missingNumber(int[] nums) {
-        int n = nums.length;
-        int total = n * (n + 1) / 2;
-        int sum = 0;
-        for (int i: nums){
-            sum += i;
-        }
-        return total - sum;
+    public static void main(String[] args) {
+        LeetCode leetCode = new LeetCode();
+    }
+    public void reverseArray(String[] nums) {
+
     }
 }
 
