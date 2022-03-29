fc6dd5451eff5063f7c4ea12f0705a1ae3f6bc3b
Hayman AbdelJabbar <hayman-35@hotmail.com>
2022-03-29 14:12:22 -0400

    add comments

--- Missing_Number/MissingNumberSol.java
+++ Missing_Number/MissingNumberSol.java
@@ -1,13 +1,16 @@
 package Missing_Number;
-
+// https://leetcode.com/problems/missing-number/
 public class MissingNumberSol {
     public int missingNumber(int[] nums) {
+        // finding the length of the array and using a formula to find the total sum of what it should be
         int n = nums.length;
-        int total = n * (n + 1) / 2;
+        int total = n * (n + 1) / 2; // the formula of how to find the total number
         int sum = 0;
-        for (int i: nums){
+
+        for (int i: nums){ // finding the sum of the array
             sum += i;
         }
-        return total - sum;
+
+        return total - sum; // the result will be what number is left out
     }
 }
