83fdbe171925a8595f473d120ab197b926bab36f
Hayman AbdelJabbar <hayman-35@hotmail.com>
2022-03-29 21:43:38 -0400

    Adding the insert into the linkedlist

--- LeetCode.java
+++ LeetCode.java
@@ -1,9 +1,9 @@
 import java.util.Arrays;
 
 public class LeetCode {
-    public static void main(String[] args) {
-        LeetCode leetCode = new LeetCode();
-    }
+//    public static void main(String[] args) {
+//        LeetCode leetCode = new LeetCode();
+//    }
     public void reverseArray(String[] nums) {
 
     }
--- SinglyLinkedList/SinglyLinkedList.java
+++ SinglyLinkedList/SinglyLinkedList.java
@@ -15,4 +15,30 @@ public class SinglyLinkedList {
         size = 1;
         return head;
     }
+
+    public void insertInLinkedList(int nodeValue, int location){
+        Node node = new Node();
+        node.value = nodeValue;
+        node.next = null;
+        if (head == null){ // list is empty so add the node to the head and tail
+            head = node;
+            tail = node;
+        }else if (location == 0){ // Inserting into the beginning of the list
+            node.next = head; // the node to reference what the head is referenecing too
+            head = node; // head to point to the node
+        }else if (location >= size){ // inserting into the tail of the node
+            node.next = null;
+            tail.next = node;
+            tail = node;
+        }else{
+            int index = 0;
+            Node tempNode = head;
+            while(index < location - 1){ // Finds the node starting from the head
+                tempNode = tempNode.next;
+                index++;
+            }
+            tempNode.next = node; // stores the next nodes reference to the new node
+            node.next = tempNode.next; // the new nodes next reference is set to the lists next node
+        }
+    }
 }
