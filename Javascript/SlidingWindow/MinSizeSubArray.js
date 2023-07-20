0225dd7bd24c7cee182c92968526b4b9d54bc078
Hayman AbdelJabbar <hayman-35@hotmail.com>
2023-07-19 22:53:14 -0400

    Finish Leetcode question

---
+++ Javascript/SlidingWindow/MinSizeSubArray.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number} target
+ * @param {number[]} nums
+ * @return {number}
+ * https://leetcode.com/problems/minimum-size-subarray-sum/
+ */
+var minSubArrayLen = function(target, nums) {
+    var start = 0;
+    var sum = 0;
+    var minLength = Infinity;
+    for(var end = 0; end < nums.length; end++){
+        sum += nums[end]
+
+        while (sum >= target){
+            minLength = Math.min(minLength, end - start + 1);
+            sum -= nums[start];
+            start++;
+        }
+    }
+    if (minLength == Infinity){
+        return 0;
+    }
+    return minLength;
+};
